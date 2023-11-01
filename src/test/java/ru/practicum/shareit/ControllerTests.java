21fd6eec040fffa40302a5393e2788f5bb600e72
Lylov <lylov@lanit.ru>
2023-11-01 13:41:25 +0300

    Спринт 15 версия 2

--- src/test/java/ru/practicum/shareit/ControllerTests.java
+++ src/test/java/ru/practicum/shareit/ControllerTests.java
@@ -4,13 +4,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @ExtendWith(MockitoExtension.class)
 @AutoConfigureMockMvc
@@ -19,15 +17,11 @@ public abstract class ControllerTests<T> {
     protected static final ObjectMapper objectMapper = new ObjectMapper();
     @Autowired
     protected final T testController;
+    @Autowired
     protected MockMvc mockMvc;
 
     @BeforeAll
     static void setDate() {
         objectMapper.registerModule(new JavaTimeModule());
-    }
-
-    @BeforeEach
-    void setUp() {
-        mockMvc = MockMvcBuilders.standaloneSetup(testController).build();
     }
 }
